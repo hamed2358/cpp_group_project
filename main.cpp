028550069912675ba014a37c2fa02fba329feb55
hamed2358 <hamedsarwari1@gmail.com>
2026-09-11 10:40:43 +0200

    Set up OpenCV camera input and test video

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
+int main()
+
+{
+
+    // Open the supplied video file.
+
+    cv::VideoCapture video("resources/test_video_fixed.mp4");
+
+    if (!video.isOpened()) {
+
+        std::cerr << "Could not open the video.\n";
+
+        return 1;
+
+    }
+
+    cv::Mat frame;
+
+    // Read and display one frame at a time.
+
+    while (video.read(frame)) {
+
+        cv::imshow("OpenCV video test", frame);
+
+        // Wait briefly; Escape closes the program.
+
+        if (cv::waitKey(20) == 27) {
+
+            break;
+
+        }
+
+    }
+
+    return 0;
+
+}
